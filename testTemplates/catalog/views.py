049dd54f82228df03c73bca2c6889b65cedfaeab
rm-000 <gardenofeve@mail.ru>
2023-12-27 18:38:09 +0300

    dz4-lektsia9-end

--- testTemplates/catalog/views.py
+++ testTemplates/catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, ProductComment
 
 menu = {'главная':'/', 'о блоге':'/about'}
 
@@ -10,12 +10,16 @@ def main_page(request):
     return render(request, "./index.html", context = data)
 
 def add_comment_page(request):
+    products = Product.objects.values('id', 'name')
     title = 'Добавить комментарий'
-    data = {'menu':menu, 'title':title}
+    data = {'menu':menu, 'title':title, 'products':products}
     return render(request, "./add_comment.html", context = data)
 
 def thanks_page(request):
     user_name = request.POST['user_name']
+    comment = request.POST['comment']
+    product = Product.objects.get(pk=request.POST['product'])
+    ProductComment.objects.create(user_name=user_name, comment=comment, product=product)
     title = 'Страница благодарности'
     data = {'menu':menu, 'title':title, 'user_name':user_name}
     return render(request, "./thanks.html", context = data)
